ac13924991dda561a678996cde42a8cfbc40894c
Netsanet Dagnachew <netsishibeshi@gmail.com>
2024-11-16 17:10:07 +0300

    banner is made

--- netflix-clone/src/utils/axios.jsx
+++ netflix-clone/src/utils/axios.jsx
@@ -1,8 +1,20 @@
- import axios from "axios";
-
+import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3",
              
 });
 
 export default instance;
+
+// const instance = axios.create({
+//     method: 'get',
+//     headers: { 'Content-Type': 'application/json'},
+//     baseURL: "https://api.themoviedb.org/3",
+//     data:
+                 
+// })
+// .then(function (response) {
+//     console.log(response);
+//   });
+
+// export default instance;
--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -1,8 +1,22 @@
-import { defineConfig } from 'vite'
+// ************DEFAULT CODE***********
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+// ***********TO CORRECT process.env  LOADING ERROR THE ABOVE CODE IS CHANGED BY THIS**********
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
- 
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY)
+    },
+    plugins: [react()],
+  }
 })
