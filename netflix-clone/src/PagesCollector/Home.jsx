d297bebe96911eb48afdd00e728182d22b72bbf8
Netsanet Dagnachew <netsishibeshi@gmail.com>
2024-11-18 00:20:43 +0300

    row component is done

--- netflix-clone/src/PagesCollector/Home.jsx
+++ netflix-clone/src/PagesCollector/Home.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Header from '../component/Header/Header';
 import Footer from '../component/Footer/Footer'
 import Banner from '../component/Banner/Banner';
-
+import RowList from '../component/Rows/RowList/RowList'
 const Home = () => {
     return (
         <div>
          <Header />
          <Banner />
+         <RowList />
          <Footer />
         </div>
     );
