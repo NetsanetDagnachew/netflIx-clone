fd136c7982c94393757c47ea5a025cd032aadeed
Netsanet Dagnachew <netsishibeshi@gmail.com>
2024-11-26 18:44:51 +0300

    vite.config.js file is changed becouse of deployment error

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -11,12 +11,13 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.API_KEY)
-    },
-    plugins: [react()],
-  }
-})
+// export default defineConfig(({ mode }) => {
+//   const env = loadEnv(mode, process.cwd(), '');
+//   return {
+//     define: {
+//       'process.env.API_KEY': JSON.stringify(env.API_KEY)
+//     },
+//     plugins: [react()],
+//   }
+// })
+export default defineConfig({ base: "/netflix-clone", plugins: [react()], }) 
